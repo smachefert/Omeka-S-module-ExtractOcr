6c7738259452e7b375734f43f81c757cd346c226
symac <web@geobib.fr>
2019-10-07 13:18:52 +0200

    Fix appel translator

--- Module.php
+++ Module.php
@@ -25,11 +25,12 @@ class Module extends AbstractModule
     public function install(ServiceLocatorInterface $serviceLocator)
     {
         $logger = $serviceLocator->get('Omeka\Logger');
+        $t = $serviceLocator->get('MvcTranslator');
         // Don't install if the pdftotext command doesn't exist.
         // See: http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
         if ((int) shell_exec('hash pdftotext 2>&- || echo 1')) {
           $logger->info("pdftotext not found");
-          throw new ModuleCannotInstallException(__('The pdftotext command-line utility '
+          throw new ModuleCannotInstallException($t->translate('The pdftotext command-line utility '
                 . 'is not installed. pdftotext must be installed to install this plugin.'));
         }
 
